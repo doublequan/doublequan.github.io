1582758de684d7396f285bea2943ecaaaa6fdace
billdqu <billdqu@gmail.com>
2017-04-10 23:05:23 -0400

    pics

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,9 +39,9 @@
             $(function () {
                 $.vegas('slideshow', {
                     backgrounds: [
-                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
-                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
-                       { src: 'assets/img/3.jpg', fade: 1000, delay: 9000 }, 
+                      { src: 'assets/img/post-bg-theory.jpg', fade: 1000, delay: 9000 }, //CHANGE THESE IMAGES WITH YOUR ORIGINAL IMAGES
+                      { src: 'assets/img/post-bg-web.jpg', fade: 1000, delay: 9000 }, //THESE IMAGES ARE FOR DEMO PURPOSE ONLY YOU, CAN NOT USE THEM WITHOUT AUTHORS PERMISSION
+                       // { src: 'assets/img/3.jpg', fade: 1000, delay: 9000 },
                      
                     ]
                 })('overlay', {
